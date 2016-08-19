e2a109466a7567315b44a3d601ec524f490608e0
doncov.eugene <doncov.eugene@alphaopen.com>
2016-08-19 21:21:16 +0300

    init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/neu-rah/ArduinoMenu.git ./lib/ArduinoMenu
+git clone https://github.com/mathertel/OneButton.git ./lib/OneButton
+curl -o ./lib/nlc.zip https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads/NewliquidCrystal_1.3.4.zip
+unzip ./lib/nlc.zip ./lib/
