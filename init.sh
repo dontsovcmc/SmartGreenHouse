f18629b5dc63f39a11f5a7b98312df44ca7356e1
doncov.eugene <doncov.eugene@alphaopen.com>
2016-08-31 14:20:46 +0300

    add stream.h to init

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/neu-rah/ArduinoMenu.git ./lib/ArduinoMenu
 git clone https://github.com/mathertel/OneButton.git ./lib/OneButton
-curl -o ./lib/nlc.zip https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads/NewliquidCrystal_1.3.4.zip
+#curl -o ./lib/nlc.zip https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads/NewliquidCrystal_1.3.4.zip
+#http://arduiniana.org/Streaming/Streaming5.zip
 unzip ./lib/nlc.zip ./lib/
